5db8a1cc954e1a1ae4f22a22ea9c1ef10d017cc6
Wolfgang Bremer <wbremer@apigear.io>
2022-07-11 10:59:51 +0200

    fix(conan): Fail bash test script on error

--- goldenmaster/test_conan.sh
+++ goldenmaster/test_conan.sh
@@ -8,53 +8,63 @@ mkdir -p apigear;
 pushd apigear;
 conan source ../../apigear && conan install --build missing ../../apigear -g=virtualenv && conan build ../../apigear && cmake ../../apigear && cmake --build . && source activate.sh && cmake --build . --target check && \
 conan install --build missing ../../apigear && conan create ../../apigear
+if [ $? -ne 0 ]; then exit 1; fi;
 popd
 conan remove "testbed2" -b -f
 mkdir -p modules/testbed2;
 pushd modules/testbed2;
 conan source ../../../modules/testbed2 && conan install --build missing ../../../modules/testbed2 -g=virtualenv && conan build ../../../modules/testbed2 && cmake ../../../modules/testbed2 && cmake --build . && source activate.sh && cmake --build . --target check && source deactivate.sh &&\
 conan install --build missing ../../../modules/testbed2 && conan create ../../../modules/testbed2
+if [ $? -ne 0 ]; then exit 1; fi;
 popd
 conan remove "tb_enum" -b -f
 mkdir -p modules/tb_enum;
 pushd modules/tb_enum;
 conan source ../../../modules/tb_enum && conan install --build missing ../../../modules/tb_enum -g=virtualenv && conan build ../../../modules/tb_enum && cmake ../../../modules/tb_enum && cmake --build . && source activate.sh && cmake --build . --target check && source deactivate.sh &&\
 conan install --build missing ../../../modules/tb_enum && conan create ../../../modules/tb_enum
+if [ $? -ne 0 ]; then exit 1; fi;
 popd
 conan remove "tb_same1" -b -f
 mkdir -p modules/tb_same1;
 pushd modules/tb_same1;
 conan source ../../../modules/tb_same1 && conan install --build missing ../../../modules/tb_same1 -g=virtualenv && conan build ../../../modules/tb_same1 && cmake ../../../modules/tb_same1 && cmake --build . && source activate.sh && cmake --build . --target check && source deactivate.sh &&\
 conan install --build missing ../../../modules/tb_same1 && conan create ../../../modules/tb_same1
+if [ $? -ne 0 ]; then exit 1; fi;
 popd
 conan remove "tb_same2" -b -f
 mkdir -p modules/tb_same2;
 pushd modules/tb_same2;
 conan source ../../../modules/tb_same2 && conan install --build missing ../../../modules/tb_same2 -g=virtualenv && conan build ../../../modules/tb_same2 && cmake ../../../modules/tb_same2 && cmake --build . && source activate.sh && cmake --build . --target check && source deactivate.sh &&\
 conan install --build missing ../../../modules/tb_same2 && conan create ../../../modules/tb_same2
+if [ $? -ne 0 ]; then exit 1; fi;
 popd
 conan remove "tb_simple" -b -f
 mkdir -p modules/tb_simple;
 pushd modules/tb_simple;
 conan source ../../../modules/tb_simple && conan install --build missing ../../../modules/tb_simple -g=virtualenv && conan build ../../../modules/tb_simple && cmake ../../../modules/tb_simple && cmake --build . && source activate.sh && cmake --build . --target check && source deactivate.sh &&\
 conan install --build missing ../../../modules/tb_simple && conan create ../../../modules/tb_simple
+if [ $? -ne 0 ]; then exit 1; fi;
 popd
 conan remove "testbed1" -b -f
 mkdir -p modules/testbed1;
 pushd modules/testbed1;
 conan source ../../../modules/testbed1 && conan install --build missing ../../../modules/testbed1 -g=virtualenv && conan build ../../../modules/testbed1 && cmake ../../../modules/testbed1 && cmake --build . && source activate.sh && cmake --build . --target check && source deactivate.sh &&\
 conan install --build missing ../../../modules/testbed1 && conan create ../../../modules/testbed1
+if [ $? -ne 0 ]; then exit 1; fi;
 popd
 # examples app
 mkdir -p examples/app;
 pushd examples/app;
 conan install --build missing ../../../examples/app -g=virtualenv && cmake ../../../examples/app && cmake --build .
+if [ $? -ne 0 ]; then exit 1; fi;
 popd
 mkdir -p examples/olinkserver;
 pushd examples/olinkserver;
 conan install --build missing ../../../examples/olinkserver -g=virtualenv && cmake ../../../examples/olinkserver && cmake --build .
+if [ $? -ne 0 ]; then exit 1; fi;
 popd
 mkdir -p examples/olinkclient;
 pushd examples/olinkclient;
 conan install --build missing ../../../examples/olinkclient -g=virtualenv && cmake ../../../examples/olinkclient && cmake --build .
+if [ $? -ne 0 ]; then exit 1; fi;
 popd
--- templates/test_conan.sh
+++ templates/test_conan.sh
@@ -8,6 +8,7 @@ mkdir -p apigear;
 pushd apigear;
 conan source ../../apigear && conan install --build missing ../../apigear -g=virtualenv && conan build ../../apigear && cmake ../../apigear && cmake --build . && source activate.sh && cmake --build . --target check && \
 conan install --build missing ../../apigear && conan create ../../apigear
+if [ $? -ne 0 ]; then exit 1; fi;
 popd
 {% for module in system.modules %}
 conan remove "{{module.name|identifier}}" -b -f
@@ -15,18 +16,22 @@ mkdir -p modules/{{module.name|identifier}};
 pushd modules/{{module.name|identifier}};
 conan source ../../../modules/{{module.name|identifier}} && conan install --build missing ../../../modules/{{module.name|identifier}} -g=virtualenv && conan build ../../../modules/{{module.name|identifier}} && cmake ../../../modules/{{module.name|identifier}} && cmake --build . && source activate.sh && cmake --build . --target check && source deactivate.sh &&\
 conan install --build missing ../../../modules/{{module.name|identifier}} && conan create ../../../modules/{{module.name|identifier}}
+if [ $? -ne 0 ]; then exit 1; fi;
 popd
 {% endfor %}
 # examples app
 mkdir -p examples/app;
 pushd examples/app;
 conan install --build missing ../../../examples/app -g=virtualenv && cmake ../../../examples/app && cmake --build .
+if [ $? -ne 0 ]; then exit 1; fi;
 popd
 mkdir -p examples/olinkserver;
 pushd examples/olinkserver;
 conan install --build missing ../../../examples/olinkserver -g=virtualenv && cmake ../../../examples/olinkserver && cmake --build .
+if [ $? -ne 0 ]; then exit 1; fi;
 popd
 mkdir -p examples/olinkclient;
 pushd examples/olinkclient;
 conan install --build missing ../../../examples/olinkclient -g=virtualenv && cmake ../../../examples/olinkclient && cmake --build .
+if [ $? -ne 0 ]; then exit 1; fi;
 popd
