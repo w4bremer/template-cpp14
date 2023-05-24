896081ec50761652fe8b516e79d0fe6b81d3035b
Wolfgang Bremer <wbremer@apigear.io>
2023-05-24 17:50:29 +0200

    refactor(mqtt): move logic into CWrapper class
    Make the client a simple interface class to access the cwrapper without
    containing any detailed mqtt logic.

--- goldenmaster/apigear/mqtt/mqttcppclient.h
+++ goldenmaster/apigear/mqtt/mqttcppclient.h
@@ -1,40 +1,14 @@
 #pragma once
 
-#include <queue>
-#include <set>
-#include <map>
-#include <memory>
-#include <mutex>
-#include <string>
-#include <atomic>
-#include <thread>
-#include <condition_variable>
 #include "nlohmann/json.hpp"
+#include "private/mqttcwrapper.h"
 #include "mqttcommon.h"
 #include "mqtttopic.h"
-
-typedef void* MQTTAsync;
 
 namespace ApiGear {
 namespace MQTT {
 
 class ISink;
-typedef std::function<void(bool connectionStatus)> OnConnectionStatusChangedCallBackFunction;
-
-class APIGEAR_MQTT_EXPORT InvokeReplyArg {
-public:
-    nlohmann::json value;
-};
-
-class APIGEAR_MQTT_EXPORT Message {
-public:
-    Topic topic {""};
-    std::string content {""}; //optional
-    Topic responseTopic {""}; //optional
-    std::string correlationData {""}; //optional
-};
-
-typedef std::function<void(InvokeReplyArg)> InvokeReplyFunc;
 
 /**
  * @brief Describes incoming part of messages of the protocol for client side.
@@ -58,10 +32,6 @@ public:
     */
     void disconnect();
 
-    void onConnected();
-    void onDisconnected();
-    void handleTextMessage(const Message& message);
-
     // subscribe to be notified about connection changes
     int subscribeToConnectionStatus(OnConnectionStatusChangedCallBackFunction callBack);
     // unsubscribe from connection changes
@@ -75,44 +45,17 @@ public:
     // client interface
     void invokeRemote(const Topic& name, const std::string& value, InvokeReplyFunc func=nullptr);
     void setRemoteProperty(const Topic& name, const std::string& value);
+
     void subscribeTopic(const Topic& name, ISink* sink);
     void unsubscribeTopic(const Topic& name, ISink* sink);
 
     void confirmSubscription(const Topic& name, ISink& sink);
     void removeSubscription(const Topic& name);
-    
-    void run();
 
-    const std::string& getClientId() const { return m_clientID; }
-private:
-    void checkForNewSubscriptions();
-    void checkForOldSubscriptions();
-    void unsubscribeAllTopics();
-    void waitForPendingMessages();
+    const std::string& getClientId() const;
 
 private:
-    std::condition_variable m_conditionVariable;
-    std::mutex m_waitForWorkMutex;
-    bool m_waitForWork { true };
-
-    std::unique_ptr<MQTTAsync> m_client;
-    std::mutex m_queueMutex;
-    std::string m_serverUrl;
-    std::string m_clientID;
-    std::thread m_thread;
-    std::queue<std::string> m_queue;
-    std::atomic<bool> m_disconnectRequested { false };
-    std::atomic<bool> m_connected { false };
-    std::mutex m_onConnectionStatusChangedCallbacksMutex;
-    std::map<int, OnConnectionStatusChangedCallBackFunction> m_onConnectionStatusChangedCallbacks;
-    std::mutex m_invokeResultsMutex;
-    std::map<int, InvokeReplyFunc> m_invokeResults;
-    std::mutex m_subscribedTopicsMutex;
-    std::multimap<Topic, ISink*, Topic> m_subscribedTopics;
-    std::mutex m_toBeSubscribedTopicsMutex;
-    std::multimap<Topic, ISink*, Topic> m_toBeSubscribedTopics;
-    std::mutex m_toBeUnsubscribedTopicsMutex;
-    std::multimap<Topic, ISink*, Topic> m_toBeUnsubscribedTopics;
+    std::unique_ptr<CWrapper> m_cwrapper;
 };
 } // namespace MQTT
 } // namespace ApiGear
